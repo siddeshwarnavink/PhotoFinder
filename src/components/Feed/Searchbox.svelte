cfa5f719e2d448ae6f8c238eb285451bdc87b5e3
Siddeshwar <siddeshwar.work@gmail.com>
2022-09-07 06:37:03 +0530

    added search box in feed page

---
+++ src/components/Feed/Searchbox.svelte
@@ -0,0 +1,68 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    import Button from "../Shared/Button.svelte";
+
+    export let query;
+
+    const dispatch = createEventDispatcher();
+
+    function onInputChangeHandler(event) {
+        dispatch("change", { value: event.target.value });
+    }
+</script>
+
+<div class="search-box">
+    <div class="search-box__input">
+        <span class="material-icons">search</span>
+        <input
+            placeholder="e.g nature"
+            value={query}
+            on:keyup={onInputChangeHandler}
+        />
+    </div>
+
+    <div class="search-box__searchBtn">
+        <Button>Search</Button>
+    </div>
+</div>
+
+<style>
+    .search-box {
+        display: flex;
+        max-width: 400px;
+        margin: auto;
+        padding: 12px 0;
+    }
+
+    .search-box .search-box__searchBtn {
+        margin-left: 12px;
+    }
+
+    .search-box .search-box__input {
+        background-color: #fff;
+        color: #000;
+        padding: 6px 12px;
+        display: flex;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+    }
+
+    .search-box__input input,
+    .search-box__input .material-icons {
+        display: block;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .search-box__input input {
+        border: 0;
+        outline: none;
+        font-size: 1.2em;
+        width: 100%;
+    }
+
+    .search-box__input .material-icons {
+        padding-right: 5px;
+    }
+</style>
--- src/pages/Feed.svelte
+++ src/pages/Feed.svelte
@@ -6,15 +6,25 @@
     import FeedGridItem from "../components/Feed/FeedGridItem.svelte";
     import FeedLightbox from "../components/Feed/FeedLightbox.svelte";
     import PageJumbotron from "../components/Shared/PageJumbotron.svelte";
+    import Searchbox from "../components/Feed/Searchbox.svelte";
 
     let isLoading = false;
+    let searchQuery = "";
     let photoFeed = [];
     let selectedFeed = {};
 
-    onMount(() => {
+    onMount(fetchFeedHandler);
+
+    function fetchFeedHandler() {
         isLoading = true;
 
-        fetch("https://api.pexels.com/v1/search?query=landscape", {
+        let url = "https://api.pexels.com/v1/search?query=" + searchQuery;
+
+        if (searchQuery.trim() === "") {
+            url = "https://api.pexels.com/v1/search?query=Nature";
+        }
+
+        fetch(url, {
             headers: {
                 Authorization:
                     "563492ad6f91700001000001d887955a8b9a4f91950ade4a232e87d8",
@@ -25,7 +35,7 @@
                 isLoading = false;
                 photoFeed = json.photos;
             });
-    });
+    }
 
     function viewFeedHandler(feedData) {
         selectedFeed = feedData;
@@ -34,12 +44,24 @@
     function closeSelectedFeedHandler() {
         selectedFeed = {};
     }
+
+    function onSearchQueryChangeHandler(event) {
+        searchQuery = event.detail.value;
+    }
+
+    function onSearchSubmitHandler() {
+        fetchFeedHandler();
+    }
 </script>
 
 <PageJumbotron
     caption="Gallery feed"
     subcaption="Our collections of quality photos!"
 />
+
+<form on:submit|preventDefault={onSearchSubmitHandler}>
+    <Searchbox query={searchQuery} on:change={onSearchQueryChangeHandler} />
+</form>
 
 <FeedLightbox
     show={Object.keys(selectedFeed).length > 0}
